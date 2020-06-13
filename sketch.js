abe0696695b8a2465b2052fbeef6d945c8499729
Keishaa <keishaavarma@gmail.com>
2020-06-13 10:32:47 +0530

    paint

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
-var canvas, bgImg;
-var database, playerCount;
-var gameState= 0; 
-var form, game, player;
+var canvas, brush;
+var database;
 
 function setup(){
     database= firebase.database();
     canvas= createCanvas(500,500);
-    game= new Game(); 
-    game.getState();
-    game.start();
+    x= database.ref('position');
+    x.on ("value", function (data){ 
+        y= data.val(); 
+    }, console.log("error"));
 }
 
 function draw(){
-    
+    brush= createSprite(250,250,50,50);
+}
+
+function update (pos){
+    database.ref('position').set({
+        x: ball.x,
+        y: ball.y
+    });
+}
+
+function mouseDragged(){
+    brush.x= mouse.x;
+    brush.y= mouse.y;
 }
